1640110abbae1abcff609bd5ffe0f872a7f637a3
maninhomarcos <111518436+maninhomarcos@users.noreply.github.com>
2023-03-01 11:18:42 -0300

    Add files via upload

---
+++ m.js
@@ -0,0 +1,15 @@
+
+function douglas(){
+    var nome = document.getElementById("nome").value;
+
+    var resposta ="Que ótima resposta!:" 
+
+    document.getElementById("resposta").innerHTML = resposta + nome;
+}
+function mk(){
+    var ls = document.getElementById("ls").value;
+
+    var res ="Otima observação!:" 
+
+    document.getElementById("res").innerHTML = res + ls;
+}
